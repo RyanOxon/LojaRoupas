d3c8b0eb78b06e445c1cdff78e9a738ee17ae353
gustavocmiranda <56138255+gustavocmiranda@users.noreply.github.com>
2021-04-30 20:40:41 -0300

    Update Seguranca.java

--- Pessoas/Seguranca.java
+++ Pessoas/Seguranca.java
@@ -2,9 +2,29 @@ package Pessoas;
 
 
 public class Seguranca extends Funcionario{
-    Seguranca(String nome, String cpf, String endereco, int telefone){
+    public Seguranca(String nome, String cpf, String endereco, int telefone){
         super(nome, cpf, endereco, telefone);
         this.funcao = "Segurança";
         this.salario = 2200;
     }  
+
+    public void checarCompras(Cliente cliente){
+        new Thread(){
+
+            public void run() {
+                try{
+                    System.out.println("Conferindo as compras realizadas...");
+                    
+                    Thread.sleep(1800);
+                    
+                    System.out.println("Checagem finalizada, " + cliente.getNome() + " está liberado(a)");
+                }catch(Exception ex){
+                    ex.getMessage();
+                }
+            
+            }
+            
+       }.start();
+    }
+
 }
