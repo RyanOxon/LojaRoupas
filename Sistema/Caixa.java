ad7b0a23b5fc42e40a6817fb0a3fd06acc9309b0
Rafael Salgado <rafael.salgado31@gmail.com>
2021-04-30 19:00:29 -0300

    Update Caixa.java

---
+++ Sistema/Caixa.java
@@ -0,0 +1,51 @@
+package Sistema;
+
+import java.util.*;
+import Itens.Produto;
+import Pessoas.Cliente;
+import Pessoas.Vendedor;
+
+public class Caixa {
+    private Vendedor usuario;
+    private Cliente clienteAtual;
+    
+    Set<Produto> carrinho = new HashSet<>();
+
+    public Caixa(Vendedor vendedor) {
+        this.setUsuario(vendedor);
+    }
+
+    public Cliente getClienteAtual() {
+        return clienteAtual;
+    }
+
+    public void setClienteAtual(Cliente clienteAtual) {
+        this.clienteAtual = clienteAtual;
+    }
+
+    public Vendedor getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Vendedor usuario) {
+        this.usuario = usuario;
+    }
+
+    public void addItem(Produto prod){
+        carrinho.add(prod);
+    }
+    
+    public void removeItem(Produto prod){
+        carrinho.remove(prod);
+    }
+    
+    public double getTotal(){
+        double total = 0;
+        
+        for(Produto prod: carrinho){
+            total += prod.getQtd()*prod.getValor();
+        }
+        
+        return total;
+    }
+}
