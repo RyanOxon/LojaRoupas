7b9b366a15490622d528e1ccd964c8ead4006031
gustavocmiranda <56138255+gustavocmiranda@users.noreply.github.com>
2021-04-30 20:12:06 -0300

    Update Estoque.java

--- Controle/Estoque.java
+++ Controle/Estoque.java
@@ -1,34 +1,22 @@
 package Controle;
 
-import Arquivos.Arquivo;
-import Itens.Produto;
+
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.io.PrintWriter;
 
 
 public class Estoque {
     private static String arquivo = "Estoque.txt";
     
-    /*
-    public void ler(){
-        for(int i = 0; i<3; i++){
-            String conteudo = Arquivo.Read(arq);
-            String descricao = conteudo.split(";")[0];
-            String qtd = conteudo.split(";")[1];
-
-            System.out.println(descricao + " " + qtd);
-        }
-        
-    }*/
     
-    public static String Read(String prod){
+    public static int ConferirEstoque(String prod, int quantidade_necessaria){
         String conteudo = "";
         String descricao;
-        String qtd;
+        String qtd_estoque;
+        int quantidade_estoque;
+        
         try {
             FileReader arq = new FileReader(arquivo);
             BufferedReader lerArq = new BufferedReader(arq);
@@ -37,36 +25,28 @@ public class Estoque {
                 while((linha = lerArq.readLine()) != null){
 
                     descricao = linha.split(";")[0];
-                    qtd = linha.split(";")[1];
+                    qtd_estoque = linha.split(";")[1];
+                    quantidade_estoque = Integer.parseInt(qtd_estoque);
 
-                    System.out.println(descricao + " " + qtd);
+                    if(prod.equals(descricao)){
+                        System.out.println(descricao + " " + qtd_estoque);
+                        if(quantidade_estoque >= quantidade_necessaria)
+                            return 1;
+                        else
+                            return 0;
+                    } 
                 }
-                
-                //linha = lerArq.readLine();
-                
                 arq.close();
-                return conteudo;
+                
             } catch (IOException ex) {
                 System.out.println("Erro: Não foi possível ler o arquivo!");
-                return "";
+                return -1;
             }
         } catch (FileNotFoundException ex) {
             System.out.println("Erro: Arquivo não encontrado!");
-            return "";
+            return -1;
         }
+        
+        return -1;
     }
-    
-    public static boolean Write(String Caminho,String Texto){
-        try {
-            FileWriter arq = new FileWriter(Caminho);
-            PrintWriter gravarArq = new PrintWriter(arq);
-            gravarArq.println(Texto);
-            gravarArq.close();
-            return true;
-        }catch(IOException e){
-            System.out.println(e.getMessage());
-            return false;
-        }
-    }
-    
 }
