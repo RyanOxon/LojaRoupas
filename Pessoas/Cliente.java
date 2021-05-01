d2242de5f6927c4b9d8601ab8bde65372bbbfbee
gustavocmiranda <56138255+gustavocmiranda@users.noreply.github.com>
2021-05-01 18:35:15 -0300

    Alteracoes em finalidade
    passei finalidade para int e add metodo maisPontosDeFinalidade para incrementar os pontos de um clinte

--- Pessoas/Cliente.java
+++ Pessoas/Cliente.java
@@ -1,22 +1,28 @@
+package Pessoas;
+
 import java.io.Serializable;
 
-//package Pessoas;
+
  
 //import Itens.Produto;
 
 public class Cliente extends Pessoa{
-    private String fidelidade;
+    private int fidelidade;
     
     
-    Cliente(String nome, String cpf, String endereco, String telefone){
+    public Cliente(String nome, String cpf, String endereco, String telefone){
         super(nome, cpf, endereco, telefone);
-        this.fidelidade = "0"; 
+        this.fidelidade = 0; 
     }
-    Cliente(String nome, String cpf, String endereco, String telefone, String fidelidade){
+    public Cliente(String nome, String cpf, String endereco, String telefone, int fidelidade){
         super(nome, cpf, endereco, telefone);
         this.fidelidade = fidelidade;
     }
-    public String getFidelidade(){
+    public int getFidelidade(){
         return this.fidelidade;
     }   
+    
+    public void maisPontoDeFidelidade(){
+        this.fidelidade++;
+    }
 }
