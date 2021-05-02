14922f775810977b512158ac9c1298a45106480c
gustavocmiranda <56138255+gustavocmiranda@users.noreply.github.com>
2021-05-02 19:16:01 -0300

    Polimento do código

--- Controle/Arquivo.java
+++ Controle/Arquivo.java
@@ -3,7 +3,6 @@ package Controle;
 import java.io.*;
 import java.util.ArrayList;
 
-import Controle.BancoDeDados;
 import Itens.Produto;
 import Pessoas.Cliente;
 import Pessoas.Seguranca;
@@ -14,64 +13,68 @@ public class Arquivo {
 
     public static void lerProdutos() {
 		try{
-			ObjectInputStream in = new ObjectInputStream(new FileInputStream("Controle\\Produtos.dat"));
+			ObjectInputStream in = new ObjectInputStream(new FileInputStream("Produtos.dat"));
 			try{
 			ArrayList<Produto> p = (ArrayList<Produto>)in.readObject();
 			in.close();
 			for(Produto produto : p) BancoDeDados.addProduto(produto);
 			} catch (ClassNotFoundException e) {
-				System.out.println("Erro ao ler a classe");
+                                //e.printStackTrace();
+                                System.out.println("Arquvio de produtos vazio!");
 			}
 		}catch(IOException e){
-			System.out.println("Erro ao ler");
+                        e.printStackTrace();
 		}	
     }
 	public static void lerVendedor() {
 		try{
-			ObjectInputStream in = new ObjectInputStream(new FileInputStream("Controle\\Vendedores.dat"));
+			ObjectInputStream in = new ObjectInputStream(new FileInputStream("Vendedores.dat"));
 			try{
 			ArrayList<Vendedor> p = (ArrayList<Vendedor>)in.readObject();
 			in.close();
 			for(Vendedor vendedor : p) BancoDeDados.addVendedor(vendedor);
 			} catch (ClassNotFoundException e) {
-				System.out.println();
+				//e.printStackTrace();
+                                System.out.println("Arquvio de vandedores vazio!");
 			}
 		}catch(IOException e){
-			System.out.println();
+			e.printStackTrace();
 		}	
     }
 	public static void lerSeguranca() {
 		try{
-			ObjectInputStream in = new ObjectInputStream(new FileInputStream("Controle\\Segurancas.dat"));
+			ObjectInputStream in = new ObjectInputStream(new FileInputStream("Segurancas.dat"));
 			try{
 			ArrayList<Seguranca> p = (ArrayList<Seguranca>)in.readObject();
 			in.close();
 			for(Seguranca seguranca : p) BancoDeDados.addSeguranca(seguranca);
 			} catch (ClassNotFoundException e) {
-				System.out.println();
+				//e.printStackTrace();
+                                System.out.println("Arquvio de seguranças vazio!");
 			}
 		}catch(IOException e){
-			System.out.println();
+			e.printStackTrace();
 		}	
 	}
 	public static void lerClientes() {
 		try{
-			ObjectInputStream in = new ObjectInputStream(new FileInputStream("Controle\\Clientes.dat"));
+			ObjectInputStream in = new ObjectInputStream(new FileInputStream("Clientes.dat"));
 			try{
 			ArrayList<Cliente> p = (ArrayList<Cliente>)in.readObject();
 			in.close();
 			for(Cliente cliente : p) BancoDeDados.addCliente(cliente);
 			} catch (ClassNotFoundException e) {
-				System.out.println();
+				//e.printStackTrace();
+                                System.out.println("Arquvio de clientes vazio!");
 			}
 		}catch(IOException e){
-			System.out.println();
+			e.printStackTrace();
 		}	
 	}
 
 	public static void escreveProduto() {
 		try {
-			ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("Controle\\Produtos.dat"));
+			ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("Produtos.dat"));
 			out.writeObject(BancoDeDados.getProduto());
 			out.flush();
 			out.close();
@@ -83,7 +86,7 @@ public class Arquivo {
 
 	public static void escreveVendedor() {
 		try {
-			ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("Controle\\Vendedores.dat"));
+			ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("Vendedores.dat"));
 			out.writeObject(BancoDeDados.getVendedor());
 			out.flush();
 			out.close();
@@ -94,7 +97,7 @@ public class Arquivo {
 
 	public static void escreveSeguranca() {
 		try {
-			ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("Controle\\Segurancas.dat"));
+			ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("Segurancas.dat"));
 			out.writeObject(BancoDeDados.getSeguranca());
 			out.flush();
 			out.close();
@@ -105,7 +108,7 @@ public class Arquivo {
 
 	public static void escreveCliente() {
 		try {
-			ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("Controle\\Clientes.dat"));
+			ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("Clientes.dat"));
 			out.writeObject(BancoDeDados.getClientes());
 			out.flush();
 			out.close();
